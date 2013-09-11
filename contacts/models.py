2762404f29c8bbcc8b7dbe6de4e1e8ff9a828d2d
Levalife <levushka14@gmail.com>
2013-09-11 22:56:57 +0300

    Cintact form translation

--- contacts/models.py
+++ contacts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.forms import ModelForm, Textarea, TextInput
 from captcha.fields import CaptchaField
+from django.utils.translation import ugettext_lazy as _
 #class Contact(models.Model):
 
 class Contact(models.Model):
-	title = models.CharField(max_length=100)
-	name = models.CharField(max_length=100)
-	mail = models.EmailField(max_length=100)
-	text = models.TextField('Message:')
+	title = models.CharField(_('Message title'), max_length=100)
+	name = models.CharField(_('Your name'), max_length=100)
+	mail = models.EmailField(_('Your email adress'), max_length=100)
+	text = models.TextField(_('Message'))
 
 	class Meta:
 		verbose_name = 'Message'
@@ -17,7 +18,8 @@ class Contact(models.Model):
 		return self.title
 
 class ContactForm(ModelForm):
-	captcha = CaptchaField(label='Confirmation code')
+	captcha = CaptchaField(label=_('Confirmation code'))
 
 	class Meta:
 		model = Contact
+		
